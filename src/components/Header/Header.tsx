0539428a8bea43a52422670747f090dcc3b92609
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-27 23:48:03 +0200

    feat: add checklist icon to header and make it change on mouse hover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import "./Header.css"
 
 export default function Header(){
+    function onMouseOverHandler(){
+        (document.getElementById("header-checklist-icon") as HTMLImageElement).src = "../../../public/checklist-hover.svg";
+    }
+
+    function onMouseLeaveHandler(){
+        (document.getElementById("header-checklist-icon") as HTMLImageElement).src = "../../../public/checklist.svg";
+    }
+
     return (
         <header>
-            <a href="/">ToDo List</a>
+            <a href="/" onMouseOver={onMouseOverHandler} onMouseLeave={onMouseLeaveHandler}>
+                <img id="header-checklist-icon" src="../../../public/checklist.svg"/>
+                ToDo List
+            </a>
         </header>
     )   
 }
