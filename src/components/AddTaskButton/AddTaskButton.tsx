96d2ecd40737b10be24673d6b87e6f51c419f048
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-30 16:53:20 +0200

    feat: change default html 'Add task' button to ANTD button

--- src/components/AddTaskButton/AddTaskButton.tsx
+++ src/components/AddTaskButton/AddTaskButton.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./AddTaskButton.css"
 import { getDefaultTask } from "../../helperFunctions/GetDummyTasks";
-import Button from "../ui/Button/Button";
 import CreateUpdateTaskFormModal from "../modals/CreateUpdateTaskFormModal/CreateUpdateTaskFormModal";
+import { Button } from "antd";
 
 export default function AddTaskButton(){
     const [isOpen, setIsOpen] = useState(false);
     // Function that handles creating form opening.
-    function openHandler(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void{
+    function openHandler(event: React.MouseEvent<HTMLElement, MouseEvent>): void{
         event.preventDefault();
         setIsOpen(true);
     }
@@ -19,7 +19,7 @@ export default function AddTaskButton(){
 
     return (
         <div className="add-task-button-holder">
-            <Button type="click" buttonClasses="add-task-button button-on-board" onClickHandler={(event) => openHandler(event)}>+</Button>
+            <Button className="add-task-button" onClick={(event) => openHandler(event)}>+</Button>
             <CreateUpdateTaskFormModal type="create" prevTaskData={getDefaultTask()} isOpen={isOpen} closeHandler={closeHandler} />
         </div>
         
